2e89e7f405edeaf172af1bf70a5ab772602f7cbe
Marcel Toele <Marcel.Toele@aimms.com>
2015-08-19 10:30:18 +0200

    AddressBook: model-dialog-request-handler is now robust with concurrent modification of the RequestQueue identifier; RE #2372 RE #2399

--- AddressBook/WebUI/resources/plugins/model-dialog-request-handler/src/jquery.aimms.model-dialog-request-handler.js
+++ AddressBook/WebUI/resources/plugins/model-dialog-request-handler/src/jquery.aimms.model-dialog-request-handler.js
@@ -3,22 +3,25 @@ jQuery.widget('aimms.model_dialog_request_handler', AWF.Widget.create({
     _create: function() {
 		console.log("CREATED");
 		this.currentRequestQueue = [];
+		this.isHandlingRequest = false;
 	},
 	onResolvedOptionChanged: function(optionName, optionValue) {
 		var widget = this;
 
 		console.log("onResolvedOptionChanged", arguments);
-		if(optionName == "contents2" && optionValue) {
+		if(!widget.isHandlingRequest && optionName == "contents2" && optionValue) {
 			console.log("WE GOT A VALUE: ", optionValue);
 			var dataSource = optionValue;
 			AWF.DataSourceUtil.getAsListOfObjects(dataSource, function(requests) {
 				widget.currentRequestQueue = requests;
 
 				var unhandledRequests = requests.filter(function(request){return request("result").value == "<empty>"});
-				console.log("req/unh:", requests, unhandledRequests);
+				console.log("req/unh:", requests.map(function(r){return r("rq").value}), unhandledRequests.map(function(r){return r("rq").value}));
 				var request = unhandledRequests[0];
 				console.log("request", request);
 				if(request) {
+					widget.isHandlingRequest = true;
+					var requestId = request("rq").value;
 					var title = request("title").value;
 					var message = request("message").value;
 					var resultRef = request("result");
@@ -27,18 +30,22 @@ jQuery.widget('aimms.model_dialog_request_handler', AWF.Widget.create({
 					var actions = Object.transform(actionsArray, function(index, actionName) {
 						return [actionName, function() {
 							console.log("requestSetValue:", actionName)
-							dataSource.values.requestSetValue(
-								[
-									{start: resultRef.position.row, end: resultRef.position.row+1},
-									{start: resultRef.position.col, end: resultRef.position.col+1}
-								],
-								"values",
-								actionName
-							);
-							// var requestInLatestResolvedValue = widget.currentRequestQueue.reduce(function(prev, request_) {
-							// 	return prev || request_("requests") == request("requests");
-							// }, undefined);
-							// console.log("requestInLatestResolvedValue", requestInLatestResolvedValue.position);
+							var currentResultRef = widget.currentRequestQueue.reduce(function(prev, cur) {
+								return prev || (cur("rq").value == requestId && cur("result"));
+							}, null);
+							var currentDataSource = widget.element.awf.resolvedOptions('contents2');
+							console.log("RWGRWGW", request("rq"), request("result"), currentResultRef);
+							if(currentDataSource) {
+								currentDataSource.values.requestSetValue(
+									[
+										{start: currentResultRef.position.row, end: currentResultRef.position.row+1},
+										{start: currentResultRef.position.col, end: currentResultRef.position.col+1}
+									],
+									"values",
+									actionName
+								);
+							}
+							widget.isHandlingRequest = false;
 						}];
 					});
 
