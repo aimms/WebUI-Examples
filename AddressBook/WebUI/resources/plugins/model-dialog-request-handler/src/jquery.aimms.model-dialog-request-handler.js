819aac20756ea5faae5eb48c4493cfd140e76d23
Marcel Toele <EleotleCram@gmail.com>
2015-08-25 12:59:23 +0200

    AddressBook: model-dialog-request-handler: Now handles at most 1 request at a time; RE #2372 RE #2399

--- AddressBook/WebUI/resources/plugins/model-dialog-request-handler/src/jquery.aimms.model-dialog-request-handler.js
+++ AddressBook/WebUI/resources/plugins/model-dialog-request-handler/src/jquery.aimms.model-dialog-request-handler.js
@@ -1,24 +1,19 @@
 // Create a base AWF Widget and register it as a jQuery UI widget:
 jQuery.widget('aimms.model_dialog_request_handler', AWF.Widget.create({
     _create: function() {
-		console.log("CREATED");
 		this.currentRequestQueue = [];
 		this.isHandlingRequest = false;
 	},
 	onResolvedOptionChanged: function(optionName, optionValue) {
 		var widget = this;
 
-		console.log("onResolvedOptionChanged", arguments);
-		if(!widget.isHandlingRequest && optionName == "contents2" && optionValue) {
-			console.log("WE GOT A VALUE: ", optionValue);
+		if(!widget.isHandlingRequest && optionName == "contents" && optionValue) {
 			var dataSource = optionValue;
 			AWF.DataSourceUtil.getAsListOfObjects(dataSource, function(requests) {
 				widget.currentRequestQueue = requests;
 
 				var unhandledRequests = requests.filter(function(request){return request("result").value == "<empty>"});
-				console.log("req/unh:", requests.map(function(r){return r("rq").value}), unhandledRequests.map(function(r){return r("rq").value}));
 				var request = unhandledRequests[0];
-				console.log("request", request);
 				if(request) {
 					widget.isHandlingRequest = true;
 					var requestId = request("rq").value;
@@ -29,12 +24,10 @@ jQuery.widget('aimms.model_dialog_request_handler', AWF.Widget.create({
 					var actionsArray = JSON.parse(actionsJson);
 					var actions = Object.transform(actionsArray, function(index, actionName) {
 						return [actionName, function() {
-							console.log("requestSetValue:", actionName)
 							var currentResultRef = widget.currentRequestQueue.reduce(function(prev, cur) {
 								return prev || (cur("rq").value == requestId && cur("result"));
 							}, null);
-							var currentDataSource = widget.element.awf.resolvedOptions('contents2');
-							console.log("RWGRWGW", request("rq"), request("result"), currentResultRef);
+							var currentDataSource = widget.element.awf.resolvedOptions('contents');
 							if(currentDataSource) {
 								currentDataSource.values.requestSetValue(
 									[
@@ -49,8 +42,6 @@ jQuery.widget('aimms.model_dialog_request_handler', AWF.Widget.create({
 						}];
 					});
 
-					REQUESTS=requests;
-					console.log("Check REQUESTS");
 					AWF.Bus.requestPerformDialog(title, message, actions);
 				}
 
